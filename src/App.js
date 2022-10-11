923b48778b9230ccd9307c63647cef07f42f976e
SoxketWrench <SocketWrench255@hotmail.com>
2022-10-11 22:54:17 +0900

    簡単処理追加バグあり画面表示なし

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+//import ReactDOM from 'react-dom';
+import createRoot from 'react-dom';
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
 
 function App() {
   return (
     <div className="App">
+      <div className = 'game'>
+        <div className='main'>
+          <Table />
+        </div>
+      </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -20,6 +31,53 @@ function App() {
       </header>
     </div>
   );
+}
+
+class Game extends React.Component{
+  render(){
+    return(
+      <div className = 'game'>
+        <div className='main'>
+          <Table />
+        </div>
+      </div>
+    )
+  }
+}
+
+root.render(
+  <Game />,
+  document.getElementById('root')
+);
+
+class Table extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = this.getInitialState();
+  }
+  getInitialState = () => {
+    let arr = Array("1","2","3","4","5","6","7","8","9","10","11","12","13");
+    let sts = Array(13).fill(0);
+    arr = this.shuffle(arr);
+    return {
+      cards: arr,
+      status: sts,
+      ready: -1,
+      message: '',
+      const: 15,
+      timer: null,
+      title: '',
+      run: false,
+      overlay: 'overlay'
+    }
+  }
+  shuffle = ([...array]) => {
+    for (let i = array.length - 1; i >= 0; i--){
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i],array[j]]=[array[j],array[i]];
+    }
+    return array;
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import createRoot from 'react-dom/client';
+//import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const container = document.getElementById('app');
+const root = createRoot(container);
+//const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
