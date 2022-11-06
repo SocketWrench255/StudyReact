4034d8e21184371f7ee6e6a36c5c17431d70435f
SocketWrench255 <85822723+SocketWrench255@users.noreply.github.com>
2022-11-06 18:20:47 +0900

    環境構築

---
+++ src/Main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div>
+        <Link to="/">Main</Link>
+      </div>
+    )
+  }
+}
+
+export default Main;
